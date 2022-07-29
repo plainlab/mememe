c55f2efa8976afa58857faca67d51b95b41ac2f8
manhtai <vo@manhtai.com>
2022-07-29 21:48:03 +0700

    Update sequelize

--- models.js
+++ models.js
@@ -2,7 +2,14 @@ require("dotenv").config();
 
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+});
 
 // Models
 const Team = sequelize.define("Team", {
